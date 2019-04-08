d747c01ecc58082b00bbf4fb9769392a035ef0d7
Donapieppo <donapieppo@yahoo.it>
2019-04-09 00:06:56 +0200

    current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,29 @@ class ApplicationController < ActionController::Base
 
   I18n.locale = :it
 
-  def pundit_user
-    User.find(1)
-  end
+  helper_method :current_user, :user_admin?, :user_admin!
+
+  before_action :log_current_user
 
   def current_user
-    User.find(1)
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      nil
+    end
+  end
+
+  def log_current_user
+    if current_user
+      logger.info "Current user: #{current_user.email}" 
+    end
+  end
+
+  def user_admin?
+    current_user && Rails.configuration.admins.include?(current_user.email)
+  end
+
+  def user_admin!
+    user_admin? or raise NotAllowed
   end
 end
