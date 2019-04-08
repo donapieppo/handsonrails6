380ebc91a2f125a4830baf470e0245a4ec3daceb
Donapieppo <donapieppo@yahoo.it>
2019-04-09 00:07:49 +0200

    reactions

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,15 +1,18 @@
 class ReactionsController < ApplicationController
 
   def toggle
-    @game = Game.find(params[:game_id])
+    if current_user
+      @game = Game.find(params[:game_id])
 
-    # like, hard, soft
-    what = params[:w] 
-    if Reaction.permitted_reaction?(what)
-      if r = current_user.reactions.where(name: what.to_s, game_id: @game.id,).first
-        r.destroy
-      else
-        r = current_user.reactions.create(name: what.to_s, game_id: @game.id)
+      # like, hard, soft
+      @what = params[:w] 
+      if Reaction.permitted_reaction?(@what)
+        if r = current_user.reactions.where(name: @what.to_s, game_id: @game.id,).first
+          r.destroy
+        else
+          r = current_user.reactions.create(name: @what.to_s, game_id: @game.id)
+        end
+        @num = @game.reactions.where(name: @what.to_s).count
       end
     end
   end
