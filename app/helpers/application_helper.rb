49ea6da036d79830ee55295d8bba6c808baa67ef
Donapieppo <donapieppo@yahoo.it>
2019-04-09 00:08:33 +0200

    padding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def color_link(color)
-    link_to content_tag(:i, '', class: 'fas fa-circle', style: "font-size: 18px; color: #{color.name}"), games_path(color_id: color.id), class: 'nav-link' 
+    link_to content_tag(:i, '', class: 'fas fa-circle', style: "font-size: 16px; color: #{color.name}"), games_path(color_id: color.id), class: 'nav-link p-1' 
   end
 end
