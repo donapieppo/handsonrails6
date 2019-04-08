a4bc89237758df2462782aa0163bb5e327da302e
Donapieppo <donapieppo@yahoo.it>
2019-04-09 00:06:46 +0200

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
 
   resources :games do
     get :qrcodes, on: :collection
+    get :show_img, on: :member
     resources :reactions do
       get :toggle, on: :collection
     end
   end
+
+  resources :users
+
+  get 'login', to: 'logins#index', as: :login
+  get 'logins/logout',               to: 'logins#logout',    as: :logout
+  get 'auth/google_oauth2',          as: 'google_login'
+  get 'auth/google_oauth2/callback', to: 'logins#google_oauth2'
 
   root to: 'home#index'
 end
