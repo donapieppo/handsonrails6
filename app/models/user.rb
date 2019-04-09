dc6430844a2fcaad268542dbb2bdd22aee29849c
Donapieppo <donapieppo@yahoo.it>
2019-04-09 21:25:36 +0200

    comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :games
   has_many :reactions
+  has_many :comments
 
   def has_reactions?(game, what)
     self.reactions.where(game_id: (game.is_a?(Game) ? game.id : game), name: what).any?
