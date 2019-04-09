8fa172a3642bcb02c447e760ee1e6d3ed3f161d8
Donapieppo <donapieppo@yahoo.it>
2019-04-09 21:25:10 +0200

    colors

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -8,11 +8,16 @@ module HomeHelper
       'black'  => "fas fa-ambulance"
   }
 
-  def color_button_link(color)
+  def bg_and_color(color)
     textcolor = 'white'
     textcolor = 'black' if (color.name == 'yellow' or color.name == 'orange')
+    [color.name, textcolor]
+  end
+
+  def color_button_link(color)
+    bgcolor, textcolor = bg_and_color(color)
     icon = "<i style='font-size: 35px' class='#{@@icons[color.name]}'></i>".html_safe
     link_to icon + ' ' + I18n.t(color.name), games_path(color_id: color.id), 
-      class: 'home-button', style: "color: #{textcolor}; background-color: #{color.name}"
+      class: 'home-button', style: "color: #{textcolor}; background-color: #{bgcolor}"
   end
 end
