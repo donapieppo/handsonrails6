5867ef52e2342b3e44af8cdd467dd1bf28e8e628
Donapieppo <donapieppo@yahoo.it>
2019-04-09 21:25:22 +0200

    game_reactions_cache

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -4,10 +4,21 @@ class Reaction < ApplicationRecord
 
   @@rtypes = [:like, :soft, :hard]
 
+  after_save :update_game_reactions_cache
+  after_destroy :update_game_reactions_cache
+
   def self.permitted_reaction?(what)
     @@rtypes.include?(what.to_sym)
   end
 
+  def self.each_types
+    @@rtypes.each do |t|
+      yield t
+    end
+  end
 
+  def update_game_reactions_cache
+    self.game.update_reactions_counts
+  end
 end
 
