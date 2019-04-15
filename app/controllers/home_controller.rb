84e092e416dcc31b80eb96624ef1279e11c19809
Donapieppo <donapieppo@yahoo.it>
2019-04-15 23:28:04 +0200

    last_games

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    @last_games = Game.order('games.created_at desc').includes(:user).limit(5)
+    unless user_manager?
+      @last_games = @last_games.to_show_to_anyone
+    end
     @last_comments = Comment.order('comments.created_at desc').includes(:user).limit(5)
   end
 
