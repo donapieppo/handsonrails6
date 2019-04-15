25f4ae048589f0963b414f72e75b8125d9b5806a
Donapieppo <donapieppo@yahoo.it>
2019-04-15 23:29:36 +0200

    cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   has_many :reactions
   has_many :comments
 
+  # alternative witch cache to self.reactions.where(game_id: (game.is_a?(Game) ? game.id : game), name: what).any?
   def has_reactions?(game, what)
-    self.reactions.where(game_id: (game.is_a?(Game) ? game.id : game), name: what).any?
+    game_id = game.is_a?(Game) ? game.id : game
+    @_cache_my_reactions_to_game ||= {}
+    @_cache_my_reactions_to_game[game_id] ||= self.reactions.where(game_id: game_id).map(&:name)
+    @_cache_my_reactions_to_game[game_id].include?(what.to_s)
   end
 
   def is_manager?
