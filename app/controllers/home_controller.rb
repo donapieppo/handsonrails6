4bbc1cbf3ec6eff8deb1678faba6cf18b62738e4
Donapieppo <donapieppo@yahoo.it>
2019-04-21 13:50:25 +0200

    to_show_to_anyone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @last_games = Game.order('games.created_at desc').includes(:user).limit(5)
+    @last_games = Game.order('games.created_at desc').includes(:user, :color).limit(5)
     unless user_manager?
       @last_games = @last_games.to_show_to_anyone
     end
     @last_comments = Comment.order('comments.created_at desc').includes(:user).limit(5)
+    unless user_manager?
+      @last_comments = @last_comments.where(comments.competition: false)
+    end
   end
 
   def ping
