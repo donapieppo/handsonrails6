da51032f2ad9ffc17a1ea4d50a6a977ea4ebbe7b
Donapieppo <donapieppo@yahoo.it>
2019-05-14 18:56:33 +0200

    konva and climbing-holds-pinner.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// 
+// it's worth learning about the ~ character when using imports as it will start a lookup from the node_modules 
 
 require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+require("konva")
+
+// ./node_modules/@fortawesome/fontawesome-free/js/fontawesome.js
+require("@fortawesome/fontawesome-free")
+import 'bootstrap';
+import HoldsPinner from 'climbing-holds-pinner.js';
+window.HoldsPinner = HoldsPinner;
+
+import './src/application.scss';
+
+// TODO refactor without window
+//import HoldsPinner from './holds_pinner';
+
+// https://rossta.net/blog/from-sprockets-to-webpack.html
