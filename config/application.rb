422f33310d45d9bdb61e63178b8a8dc4a2c6651e
Donapieppo <donapieppo@yahoo.it>
2019-05-19 10:04:42 +0200

    locale

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Eden
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :it
     config.hosts << "tester.dm.unibo.it"
 
     # Settings in config/environments/* take precedence over those specified here.
