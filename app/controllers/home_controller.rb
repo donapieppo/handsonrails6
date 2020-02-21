df471656a3116fee0d3bf1e985a94063ecde8cc3
Donapieppo <donapieppo@yahoo.it>
2020-02-21 17:04:51 +0100

    order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    @last_games = Game.order('games.created_at desc').includes(:user, :color).limit(6)
+    @last_games = Game.order('games.created_at desc').includes(:user, :color).limit(10)
     unless user_manager?
       @last_games = @last_games.to_show_to_anyone
     end
-    @last_comments = Comment.order('comments.created_at desc').includes(:game, :user).limit(5)
+    @last_comments = Comment.order('comments.created_at desc').includes(:game, :user).limit(10)
     unless user_manager?
       @last_comments = @last_comments.where('games.competition != 1').references(:game)
     end
