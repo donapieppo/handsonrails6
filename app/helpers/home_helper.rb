33f54cc97a6c2f1c7c4c947ffafaa09f1f56607a
Donapieppo <donapieppo@yahoo.it>
2020-03-04 18:46:42 +0100

    helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
   def color_button_link(color)
     bgcolor, textcolor = color.bg_and_color
     icon = "<i style='font-size: 35px' class='#{color.icon}'></i>".html_safe
-    link_to icon + '<br/>'.html_safe + I18n.t(color.name), games_path(color_id: color.id), 
+    link_to icon + '<br/>'.html_safe + I18n.t(color.name), color_games_path(color.id), 
       class: 'home-button', style: "color: #{textcolor}; background-color: #{bgcolor}"
   end
 end
