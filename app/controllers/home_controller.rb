7bc64560b8c20498e08ee7f663c574c2655bf47e
Donapieppo <donapieppo@yahoo.it>
2020-03-04 18:46:26 +0100

    video

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-    @last_games = Game.order('games.created_at desc').includes(:user, :color).limit(10)
+    @last_games = Game.order('games.created_at desc').includes(:user, :color).limit(15)
     unless user_manager?
       @last_games = @last_games.to_show_to_anyone
     end
-    @last_comments = Comment.order('comments.created_at desc').includes(:game, :user).limit(10)
+    @last_comments = Comment.order('comments.created_at desc').includes(:game, :user).limit(15)
     unless user_manager?
       @last_comments = @last_comments.where('games.competition != 1').references(:game)
     end
+    @last_video_games = Game.order('games.video_at desc').where('LENGTH(games.video_url) > 0')
   end
 
   def ping
