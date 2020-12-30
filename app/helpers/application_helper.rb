14827452cb5ebab80476b567f85506d2ada9c02f
Donapieppo <donapieppo@yahoo.it>
2020-12-30 10:11:57 +0100

    typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,7 +46,7 @@ module ApplicationHelper
   end
 
   def icon(i, txt='', size: 16, bold: false)
-    "<i class='#{i}' style='font-weigh: #{bold ? 'bold': 'normal'} font-size: #{size}px'></i>".html_safe + ' ' + txt
+    "<i class='#{i}' style='font-weigh: #{bold ? 'bold': 'normal'}; font-size: #{size}px'></i>".html_safe + ' ' + txt
   end
 
 end
